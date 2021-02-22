3a8bada01be1ad45b7defb8151722ff6820899ee
Ivan Kondratyev <zm.fut@bk.ru>
2021-02-22 17:18:36 +0300

    fixNameLength

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -13,8 +13,8 @@ router.post(
   "/",
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(15),
-      link: Joi.string().required().min(5).max(40),
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().min(5).max(80),
     }),
   }),
   createCard
