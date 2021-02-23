ec8383bfe466bb8d585ce9d55e05996260486ab2
Ivan Kondratyev <zm.fut@bk.ru>
2021-02-23 19:06:33 +0300

    f

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -14,7 +14,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().min(5).max(100),
+      link: Joi.string().required().min(5),
     }),
   }),
   createCard
