e6102ee9fbee456a72680588a84ff8ecb3a85412
Ivan Kondratyev <zm.fut@bk.ru>
2021-02-25 21:53:27 +0300

    fix

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -51,7 +51,7 @@ const likeCard = (req, res, next) => {
       if (!card) {
         throw new NotFoundError("Карточка не найдена");
       } else {
-        return res.status(200).send( req.user_id );
+        return res.status(200).send( card );
       }
     })
     .catch(next);
@@ -68,7 +68,7 @@ const dislikeCard = (req, res) => {
       if (!card) {
         throw new NotFoundError("Карточка не найдена");
       } else {
-        return res.ststus(500).send({ messege: `${err.messege}` });
+        return res.status(200).send( card );
       }
     })
     .catch(next);
