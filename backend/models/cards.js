2b0e8a2edd571d6446c65cecabd6abc95aa5c44a
Ivan Kondratyev <zm.fut@bk.ru>
2021-02-26 20:10:35 +0300

    fix schema

--- backend/models/cards.js
+++ backend/models/cards.js
@@ -26,12 +26,10 @@ const cardSchema = new mongoose.Schema({
       ref: "user",
     },
   },
-  likes: [
-    {
+  likes:{
       type: mongoose.Schema.Types.ObjectId,
       default: [],
     },
-  ],
   createdAt: {
     type: Date,
     default: Date.now,
